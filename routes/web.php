be14dec8754a32a23d5f81d7847e034d6de622cc
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-06-30 12:56:14 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,17 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
-Route::get('/', 'PerroController@index');
-Route::post('/perros', 'PerroController@store');
+Route::get('/', 'PerrosController@index');
+Route::get('/perros', 'PerrosController@index')->name('perros.index');
+Route::post('/perros', 'PerrosController@store')->name('perros.store');
+Route::get('/perros/create', 'PerrosController@create')->name('perros.create');
+Route::get('/perros/{perro}', 'PerrosController@show')->name('perros.show');
+Route::get('/perros/{perro}/edit', 'PerrosController@edit')->name('perros.edit');
+Route::put('/perros/{perro}', 'PerrosController@update')->name('perros.update');
+Route::delete('/perros/{perro}', 'PerrosController@destroy')->name('perros.destroy');
+
+
